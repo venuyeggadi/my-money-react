4dc4c5afeec2b9f498b9d91484023c048cf4602e
venuyeggadi <venuyeggadi@users.noreply.github.com>
2022-10-21 16:22:17 +0000

    deploy: 4df6dc38b72260ad334a5d2610870583312b1314

---
+++ static/js/main.d4a661b5.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpmy-money-react"]=this["webpackJsonpmy-money-react"]||[]).push([[0],{10:function(e,t,n){"use strict";n.r(t);var c=n(1),r=n.n(c),o=n(3),s=n.n(o),a=(n(8),n(0));var i=function(){return Object(a.jsx)("div",{className:"App",children:"Hello"})};s.a.render(Object(a.jsx)(r.a.StrictMode,{children:Object(a.jsx)(i,{})}),document.getElementById("root"))},8:function(e,t,n){}},[[10,1,2]]]);
+//# sourceMappingURL=main.d4a661b5.chunk.js.map
