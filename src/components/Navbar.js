7fae304ec11cf3ab295acf618584dd8e954e8c54
Venu Yeggadi <venu.yeggadi@thoughtworks.com>
2022-10-24 00:32:19 +0530

    Hide login, signup links when a used is logged in

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom';
+import { useAuthContext } from '../hooks/useAuthContext';
 import { useLogout } from '../hooks/useLogout'
 
 // styles
@@ -6,16 +7,28 @@ import styles from './Navbar.module.css';
 
 const Navbar = () => {
   const { logout } = useLogout()
+  const { user } = useAuthContext();
 
   return (
     <nav className={styles.navbar}>
       <ul>
         <li className={styles.title}><Link to="/">myMoney</Link></li>
-        <li><Link to="/login">Login</Link></li>
-        <li><Link to="/signup">Signup</Link></li>
-        <li>
-          <button className="btn" onClick={logout}>Logout</button>
-        </li>
+
+        {!user && (
+          <>
+            <li><Link to="/login">Login</Link></li>
+            <li><Link to="/signup">Signup</Link></li>
+          </>
+        )}
+
+        {user && (
+          <>
+            <li>Hello, { user.displayName }</li>
+            <li>
+              <button className="btn" onClick={logout}>Logout</button>
+            </li>
+          </>
+        )}
       </ul>
     </nav>
   );
