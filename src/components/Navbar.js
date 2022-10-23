21251fd67762d0691230feaf7a6891769029c978
Venu Yeggadi <venu.yeggadi@thoughtworks.com>
2022-10-23 23:36:32 +0530

    Dispatch logout action when user clicks on logout button on navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,21 @@
 import { Link } from 'react-router-dom';
+import { useLogout } from '../hooks/useLogout'
 
 // styles
 import styles from './Navbar.module.css';
 
 const Navbar = () => {
+  const { logout } = useLogout()
+
   return (
     <nav className={styles.navbar}>
       <ul>
         <li className={styles.title}><Link to="/">myMoney</Link></li>
         <li><Link to="/login">Login</Link></li>
         <li><Link to="/signup">Signup</Link></li>
+        <li>
+          <button className="btn" onClick={logout}>Logout</button>
+        </li>
       </ul>
     </nav>
   );
