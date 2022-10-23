be27f1eef2baeec2d84b94b29feb889bf2024139
Venu Yeggadi <venu.yeggadi@thoughtworks.com>
2022-10-24 01:03:21 +0530

    Check whether a user is already logged in or not and conditionally render home or login page

--- src/App.js
+++ src/App.js
@@ -3,24 +3,29 @@ import Home from './pages/home/Home';
 import Login from "./pages/login/Login";
 import Signup from "./pages/signup/Signup";
 import Navbar from "./components/Navbar";
+import { useAuthContext } from "./hooks/useAuthContext";
 
 function App() {
+  const { authIsReady } = useAuthContext();
+
   return (
     <div className='App'>
-      <BrowserRouter basename={process.env.PUBLIC_URL}>
-        <Navbar />
-        <Switch>
-          <Route exact path="/">
-            <Home></Home>
-          </Route>
-          <Route path={"/login"}>
-            <Login></Login>
-          </Route>
-          <Route path="/signup">
-            <Signup></Signup>
-          </Route>
-        </Switch>
-      </BrowserRouter>
+      {authIsReady && ( // or conditionally render children in AuthContextProvider
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
+          <Navbar />
+          <Switch>
+            <Route exact path="/">
+              <Home></Home>
+            </Route>
+            <Route path={"/login"}>
+              <Login></Login>
+            </Route>
+            <Route path="/signup">
+              <Signup></Signup>
+            </Route>
+          </Switch>
+        </BrowserRouter>
+      )}
     </div>
   );
 }
