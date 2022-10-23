d21e3425d07c8d8cb00025c7adc7101ebcfbb7a5
Venu Yeggadi <venu.yeggadi@thoughtworks.com>
2022-10-23 09:41:51 +0530

    Setup routes for home, login, signup pages and create a navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Home from './pages/home/Home';
+import Login from "./pages/login/Login";
+import Signup from "./pages/signup/Signup";
+import Navbar from "./components/Navbar";
+
 function App() {
   return (
     <div className='App'>
-      Hello
+      <BrowserRouter>
+        <Navbar />
+        <Switch>
+          <Route exact path="/">
+            <Home></Home>
+          </Route>
+          <Route path={"/login"}>
+            <Login></Login>
+          </Route>
+          <Route path="/signup">
+            <Signup></Signup>
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
