da6018103b874bcb61e2b218cdc6e7e200cdb743
Venu Yeggadi <venu.yeggadi@thoughtworks.com>
2022-10-24 15:53:16 +0530

    Fetch and show transactions of a logged user on the homepage

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,18 +1,25 @@
-import { useAuthContext } from '../../hooks/useAuthContext'; 
+import { useAuthContext } from '../../hooks/useAuthContext';
+import { useCollection } from '../../hooks/useCollection';
 
 // styles
 import styles from './Home.module.css';
 
 // components
 import TransactionForm from './TransactionForm';
+import TransactionList from './TransactionList';
 
 const Home = () => {
-  const { user } = useAuthContext(); 
+  const { user } = useAuthContext();
+  const { documents, error } = useCollection(
+    'transactions',
+    ["uid", "==", user.uid]
+  );
 
   return (
     <div className={styles.container}>
       <div className={styles.content}>
-        Transaction List
+        {error && <p>{error}</p>}
+        {!error && documents && <TransactionList transactions={documents} />}
       </div>
       <div className={styles.sidebar}>
         <TransactionForm uid={user.uid} />
@@ -20,5 +27,5 @@ const Home = () => {
     </div>
   );
 }
- 
+
 export default Home;
