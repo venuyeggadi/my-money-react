e6400ab59ee215eb08a40473c0b542fb489d1800
Venu Yeggadi <venu.yeggadi@thoughtworks.com>
2022-10-24 16:40:42 +0530

    Add delete button on each transaction to delete a transaction

--- src/hooks/useFirestore.js
+++ src/hooks/useFirestore.js
@@ -12,8 +12,10 @@ const firestoreReducer = (state, action) => {
   switch (action.type) {
     case 'IS_PENDING':
       return { isPending: true, document: null, success: false, error: null }
-    case 'DOCUMENT_ADDED':
+    case 'ADDED_DOCUMENT':
       return { isPending: false, document: action.payload, success: true, error: null }
+    case 'DELETED_DOCUMENT':
+      return { isPending: false, document: null, success: true, error: null }
     case 'ERROR':
       return { isPending: false, document: null, success: false, error: action.payload }
     default:
@@ -39,14 +41,20 @@ export const useFirestore = (collection) => {
     try {
       const createdAt = timestamp.fromDate(new Date());
       const addedDocument = await ref.add({ ...doc, createdAt });
-      dispatchIfNotCancelled({ type: 'DOCUMENT_ADDED', payload: addedDocument })
+      dispatchIfNotCancelled({ type: 'ADDED_DOCUMENT', payload: addedDocument })
     } catch (err) {
       dispatchIfNotCancelled({ type: 'ERROR', payload: err.message })
     }
   }
 
   const deleteDocument = async (id) => {
-
+    dispatch({ type: 'IS_PENDING' })
+    try {
+      await ref.doc(id).delete()
+      dispatchIfNotCancelled({ type: 'DELETED_DOCUMENT' }) 
+    } catch {
+      dispatchIfNotCancelled({ type: 'ERROR', payload: 'Could not delete'})
+    }
   }
 
   useEffect(() => {
